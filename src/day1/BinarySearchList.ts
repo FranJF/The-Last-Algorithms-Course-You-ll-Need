0f905dda718c675bf1c3f549459f03ce9af1522f
FranJF <franjcfernandez@gmail.com>
2023-07-20 13:26:52 +0200

    docs: BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,14 @@
+/* 
+Se trata de un algoritmo de búsqueda binaria en una lista ordenada. No es el más eficiente, pero es el más sencillo de implementar.
+Unicamente puede ser usado en listas ordenadas.
+Complejidad: O(log n).
+
+Funciona de la siguiente manera: divide la lista en dos mitades y comprueba si el elemento buscado está en la mitad izquierda o derecha.
+Si está en la mitad izquierda, se repite el proceso en esa mitad, y si está en la derecha, se repite el proceso en esa mitad.
+Se repite el proceso hasta que el elemento buscado se encuentra o hasta que la lista se reduce a cero.
+Si el elemento buscado no se encuentra, se devuelve false.
+Si el elemento buscado se encuentra, se devuelve true.
+*/
 export default function bs_list(haystack: number[], needle: number): boolean {
     let low = 0;
     let high = haystack.length;
